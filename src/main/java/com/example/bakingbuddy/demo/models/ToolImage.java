c449c145da6b61478773868789eb7c938e758686
Unknown <blossomperez18@gmail.com>
2020-12-10 16:42:18 -0600

    Add tool image class

---
+++ src/main/java/com/example/bakingbuddy/demo/models/ToolImage.java
@@ -0,0 +1,55 @@
+package com.example.bakingbuddy.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tool_images")
+public class ToolImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Tool tool;
+
+    public ToolImage() {}
+
+    public ToolImage(long id, String imageUrl, Tool tool) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.tool = tool;
+    }
+
+    public ToolImage(String imageUrl, Tool tool) {
+        this.imageUrl = imageUrl;
+        this.tool = tool;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Tool getTool() {
+        return tool;
+    }
+
+    public void setTool(Tool tool) {
+        this.tool = tool;
+    }
+}

5c61c820d4e3877ad78de99a6ba761076f2ee3e5
armandojsalazar <armando.salazar89@gmail.com>
2020-12-10 11:48:01 -0600

    feat: inital commit

---
+++ src/test/java/com/example/bakingbuddy/demo/BakingBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bakingbuddy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakingBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

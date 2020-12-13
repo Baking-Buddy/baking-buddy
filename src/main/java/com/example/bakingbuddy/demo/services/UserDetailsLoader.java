9ccdde8c235c1deb560da80d2320598a7e5fe070
Unknown <blossomperez18@gmail.com>
2020-12-13 14:57:54 -0600

    User Authentication

---
+++ src/main/java/com/example/bakingbuddy/demo/services/UserDetailsLoader.java
@@ -0,0 +1,27 @@
+package com.example.bakingbuddy.demo.services;
+
+import com.example.bakingbuddy.demo.Model.User;
+import com.example.bakingbuddy.demo.Model.UserWithRoles;
+import com.example.bakingbuddy.demo.Repos.Users;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsLoader implements UserDetailsService {
+
+    private final Users users;
+
+    public UserDetailsLoader(Users users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = users.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
+}

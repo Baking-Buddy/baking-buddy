499170efc75752b53aecc8f9e23578216e34ce78
Austin <austinvarnon@gmail.com>
2020-12-14 12:13:46 -0600

    FEAT: order creation
    can create and store orders, 
    WIP: sorting out the correct ID's

--- src/main/java/com/example/bakingbuddy/demo/controllers/OrderController.java
+++ src/main/java/com/example/bakingbuddy/demo/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.example.bakingbuddy.demo.controllers;
 
 import com.example.bakingbuddy.demo.Model.Order;
+import com.example.bakingbuddy.demo.Model.User;
 import com.example.bakingbuddy.demo.Repos.OrderRepository;
 import com.example.bakingbuddy.demo.Repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class OrderController {
@@ -17,10 +21,29 @@ public class OrderController {
     @Autowired
     private UserRepository userDao;
 
+//    public OrderController(OrderRepository orderDao, UserRepository userDao){
+//        this.orderDao = orderDao;
+//        this.userDao = userDao;
+//    }
+
+    @GetMapping("/orders/{id}")
+    public String order(@PathVariable long id, Model viewModel){
+        viewModel.addAttribute("order", orderDao.getOne(id));
+        return "orders/customer-order";
+    }
+
     @GetMapping("/orders/create")
     public String showOrderForm(Model viewModel){
         viewModel.addAttribute("order", new Order());
         return "orders/create";
+    }
+
+    @PostMapping("/orders/create")
+    public String createOrder(@ModelAttribute Order orderToBeSaved){
+        User userDb = userDao.getOne(1L);
+        orderToBeSaved.setOwner(userDb);
+        Order dbOrder = orderDao.save(orderToBeSaved);
+        return "redirect:/orders/" + dbOrder.getId();
     }
 
 

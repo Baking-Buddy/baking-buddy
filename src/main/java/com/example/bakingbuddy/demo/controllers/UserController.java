d4e5c31d6891c31e16a72155fccb9383eac19326
Armando Salazar <armando.salazar89@gmail.com>
2020-12-14 12:12:09 -0600

    "feat: added boolean to the getMapping and PostMapping"

--- src/main/java/com/example/bakingbuddy/demo/controllers/UserController.java
+++ src/main/java/com/example/bakingbuddy/demo/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -19,12 +20,16 @@ public class UserController {
 
     @GetMapping("/register")
     public String showRegisterForm(Model model) {
+        boolean is_baker = false;
+        model.addAttribute("is_baker", is_baker);
         model.addAttribute("user", new User());
         return"users/register";
     }
 
     @PostMapping("/register")
-    public String saveUser(@ModelAttribute User user) {
+    public String saveUser(@ModelAttribute User user,
+                           @RequestParam(required = false) boolean isBaker) {
+        user.setBaker(isBaker);
         usersDao.save(user);
         return "redirect:/login";
     }

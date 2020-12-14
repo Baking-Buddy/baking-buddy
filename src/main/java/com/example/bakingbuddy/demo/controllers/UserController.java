a08170e42032278a683bc22d6165dd3fb7c882c7
Armando Salazar <armando.salazar89@gmail.com>
2020-12-14 11:06:06 -0600

    "feat: created a register form html and changed URL from /sign-up to /register"

--- src/main/java/com/example/bakingbuddy/demo/controllers/UserController.java
+++ src/main/java/com/example/bakingbuddy/demo/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.example.bakingbuddy.demo.controllers;
 
 import com.example.bakingbuddy.demo.Model.User;
 import com.example.bakingbuddy.demo.Repos.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,21 +13,20 @@ public class UserController {
     private UserRepository usersDao;
 
     public UserController(UserRepository usersDao) {
+
         this.usersDao = usersDao;
     }
 
-    @GetMapping("/sign-up")
-    public String showUsers(Model model) {
-        model.addAttribute("users", usersDao.findAll());
-        return"users";
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("user", new User());
+        return"users/register";
     }
 
-//    @PostMapping("/sign-up")
-//    public String saveUser(@ModelAttribute User user) {
-//        String hash = passwordEncoder.encoder(user.getPassword());
-//        user.setPassword(hash);
-//        usersDao.save(user);
-//        return "redirect:/login";
-//    }
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user) {
+        usersDao.save(user);
+        return "redirect:/login";
+    }
 
 }

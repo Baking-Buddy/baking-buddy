1f4f33d72462b7db72b20749c997a657685a3de1
Unknown <blossomperez18@gmail.com>
2020-12-17 15:07:05 -0600

    Take out useless code

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,20 +1,19 @@
-const client = filestack.init(key);
+const client = filestack.init(stackKey);
 
 const options = {
     onUploadDone: updateForm,
     maxSize: 10 * 1024 * 1024,
     accept: 'image/*',
     uploadInBackground: false
+
 };
 const picker = client.picker(options);
 
 // Get references to the DOM elements
 
-const form = document.getElementById('pick-form');
 const fileInput = document.getElementById('fileupload');
 const filePicker = document.getElementById('picker');
-const nameBox = document.getElementById('nameBox');
-const urlBox = document.getElementById('urlBox');
+
 
 // Add our event listeners
 
@@ -23,25 +22,19 @@ filePicker.addEventListener('click', function (e) {
     picker.open();
 });
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    // on change
-    alert('Submitting: ' + fileInput.value);
-});
-
 // Helper to overwrite the field input value
 
 function updateForm (result) {
     const fileData = result.filesUploaded[0];
     fileInput.value = fileData.url;
-    document.querySelector("#fstackImageID").value = fileData.url;
+    document.querySelector("fileupload").value = fileData.url;
 
     // Some ugly DOM code to show some data.
-    const name = document.createTextNode('Selected: ' + fileData.filename);
-    const url = document.createElement('a');
-    url.href = fileData.url;
-    url.appendChild(document.createTextNode(fileData.url));
-    nameBox.appendChild(name);
-    urlBox.appendChild(document.createTextNode('Uploaded to: '));
-    urlBox.appendChild(url);
+    // const name = document.createTextNode('Selected: ' + fileData.filename);
+    // const url = document.createElement('a');
+    // url.href = fileData.url;
+    // url.appendChild(document.createTextNode(fileData.url));
+    // nameBox.appendChild(name);
+    // urlBox.appendChild(document.createTextNode('Uploaded to: '));
+    // urlBox.appendChild(url);
 };

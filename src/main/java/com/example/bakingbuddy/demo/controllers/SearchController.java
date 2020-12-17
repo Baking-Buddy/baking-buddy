fa3b60175dc742aec2b29f9b2277b396d656cbd3
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-12-17 15:33:44 -0600

    Feat: Added a search bar to the nav to find bakers

---
+++ src/main/java/com/example/bakingbuddy/demo/controllers/SearchController.java
@@ -0,0 +1,38 @@
+package com.example.bakingbuddy.demo.controllers;
+
+import com.example.bakingbuddy.demo.Model.User;
+import com.example.bakingbuddy.demo.Repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SearchController {
+    @Autowired
+    private UserRepository usersDao;
+
+//    @GetMapping("/search-results")
+//    public String getSearchResults(){
+//        return "home/search-results";
+//    }
+
+    @GetMapping("/search-results")
+    public String bakerSearchResults(@RequestParam(name = "query") String query, Model model){
+        List<User> userResults = usersDao.findByUsernameLike("%" + query + "%");
+        List<User> refinedUserResults = new ArrayList<>();
+        for (User baker : userResults){
+          if (baker.isBaker()){
+              refinedUserResults.add(baker);
+          }
+        }
+        model.addAttribute("userResults", refinedUserResults);
+        return "home/search-results";
+    }
+}

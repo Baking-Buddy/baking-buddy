553e1ab5485193225cfa84906b136365f050e103
Austin <austinvarnon@gmail.com>
2020-12-18 15:56:36 -0600

    baker profiles
    Baker profiles done

--- src/main/java/com/example/bakingbuddy/demo/controllers/UserController.java
+++ src/main/java/com/example/bakingbuddy/demo/controllers/UserController.java
@@ -6,10 +6,7 @@ import com.example.bakingbuddy.demo.services.EmailService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
@@ -58,6 +55,12 @@ public class UserController {
     }
 
 
+    @GetMapping("/baker-profile/{id}")
+    public String showBakerProfile(@PathVariable long id, Model model){
+        User user = usersDao.getOne(id);
+        model.addAttribute("user", user);
+        return "users/baker-profile";
+    }
 
 
 

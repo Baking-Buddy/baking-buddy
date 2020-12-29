c2e6bace2ec17f6b27fcb01e2c7b27637fd2e64a
Armando Salazar <armando.salazar89@gmail.com>
2020-12-29 15:23:52 -0600

    "feat: added rating, title, and Date to review model"

--- src/main/java/com/example/bakingbuddy/demo/Model/Review.java
+++ src/main/java/com/example/bakingbuddy/demo/Model/Review.java
@@ -1,7 +1,10 @@
 package com.example.bakingbuddy.demo.Model;
 
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name= "reviews")
@@ -10,15 +13,30 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
     @Column(nullable = false, length = 255)
     private String body;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
 
     @ManyToOne
     private User owner;
 
-    public Review(long id, String body) {
+    public Review(){}
+
+    public Review(long id, int rating, String title, String body, Date date) {
         this.id = id;
+        this.rating = rating;
+        this.title = title;
         this.body = body;
+        this.date = date;
     }
 
     public long getId() {
@@ -29,12 +47,36 @@ public class Review {
         this.id = id;
     }
 
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getBody() {
         return body;
     }
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 
     public User getOwner() {

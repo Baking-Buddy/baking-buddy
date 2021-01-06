0695a62e6589062349f79d3e2737a4d0ed22e78e
Unknown <blossomperez18@gmail.com>
2021-01-06 10:29:17 -0600

    Correct syntax error

--- src/main/java/com/example/bakingbuddy/demo/controllers/UserController.java
+++ src/main/java/com/example/bakingbuddy/demo/controllers/UserController.java
@@ -3,8 +3,10 @@ package com.example.bakingbuddy.demo.controllers;
 import com.example.bakingbuddy.demo.Model.Image;
 import com.example.bakingbuddy.demo.Model.User;
 import com.example.bakingbuddy.demo.Repos.ImageRepository;
+import com.example.bakingbuddy.demo.Repos.OrderRepository;
 import com.example.bakingbuddy.demo.Repos.UserRepository;
 import com.example.bakingbuddy.demo.services.EmailService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +14,19 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
-    private UserRepository usersDao;
+    private UserRepository userDao;
     private ImageRepository imageDao;
     private PasswordEncoder passwordEncoder;
     private final EmailService emailService;
+    private OrderRepository orderDao;
 
-    public UserController(UserRepository usersDao, PasswordEncoder passwordEncoder, EmailService emailService, ImageRepository imageDao) {
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder, EmailService emailService, ImageRepository imageDao, OrderRepository orderDao) {
 
-        this.usersDao = usersDao;
+        this.userDao = userDao;
         this.passwordEncoder = passwordEncoder;
         this.emailService = emailService;
         this.imageDao = imageDao;
+        this.orderDao = orderDao;
     }
 
     @GetMapping("/register")
@@ -36,6 +40,10 @@ public class UserController {
 
     @GetMapping("/dashboard")
     public String showDashboard(Model model) {
+        User sessionUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userDb = userDao.getOne(sessionUser.getId());
+        model.addAttribute("pendingOrders", orderDao.findAll());
+        model.addAttribute("user", userDb);
         return "users/dashboard";
     }
 
@@ -48,7 +56,7 @@ public class UserController {
         userToBeSaved.setBaker(isBaker);
         String hashPassword = passwordEncoder.encode(userToBeSaved.getPassword());
         userToBeSaved.setPassword(hashPassword);
-        User dbUser = usersDao.save(userToBeSaved);
+        User dbUser = userDao.save(userToBeSaved);
         Image profileImage = new Image(true, uploadedImage, dbUser);
         imageDao.save(profileImage);
         emailService.userCreatedProfileEmail(dbUser, "Registration", "Congratulations on setting up your Baking Buddy profile!");
@@ -58,14 +66,14 @@ public class UserController {
 
     @GetMapping("/")
     public String showHomePage(Model model){
-        model.addAttribute("users", usersDao.findAll());
+        model.addAttribute("users", userDao.findAll());
         return "home/index";
     }
 
 
     @GetMapping("/baker-profile/{id}")
     public String showBakerProfile(@PathVariable long id, Model model){
-        User user = usersDao.getOne(id);
+        User user = userDao.getOne(id);
         model.addAttribute("user", user);
         return "users/baker-profile";
     }

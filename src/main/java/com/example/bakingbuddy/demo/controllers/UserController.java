07ce4d798364cb09b39c458007268b2334609dd9
Armando Salazar <armando.salazar89@gmail.com>
2021-01-08 15:17:52 -0600

    "feat: Changed the redirect on line 88 to the home page after your register."

--- src/main/java/com/example/bakingbuddy/demo/controllers/UserController.java
+++ src/main/java/com/example/bakingbuddy/demo/controllers/UserController.java
@@ -98,7 +98,7 @@ public class UserController {
         Image profileImage = new Image(true, uploadedImage, dbUser);
         imageDao.save(profileImage);
         emailService.userCreatedProfileEmail(dbUser, "Registration", "Congratulations on setting up your Baking Buddy profile!");
-        return "redirect:/login";
+        return "redirect:/";
     }
 
     @GetMapping("/user/{id}/edit")

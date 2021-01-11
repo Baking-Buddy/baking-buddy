404570b30a5ff097c5a75b9f2e2cbfc0a13ef7bc
Austin <austinvarnon@gmail.com>
2021-01-11 16:59:19 -0600

    FEAT: Email sent on Accepted Orders

--- src/main/java/com/example/bakingbuddy/demo/controllers/OrderController.java
+++ src/main/java/com/example/bakingbuddy/demo/controllers/OrderController.java
@@ -138,6 +138,10 @@ public String showOrders(@Param("query") String query, Model model) {
         Order orderToAccept = orderDao.getOne(id);
         orderToAccept.setStatus(OrderStatus.ACCEPTED);
         orderDao.save(orderToAccept);
+
+        User emailReciever = orderToAccept.getOwner();
+        String emailSubject = "Order status has been changed";
+        mailgunService.sendSimpleMessage(emailReciever, emailSubject, "Order has been " + orderToAccept.getStatus());
         return "redirect:/orders";
     }
 
